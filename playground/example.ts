60da45e4228a48008f7e307af50e9126e06ea3a6
Дамјан Георгиевски <gdamjan@gmail.com>
2023-12-31 16:54:11 +0100

    fix example.ts

--- playground/example.ts
+++ playground/example.ts
@@ -3,6 +3,6 @@ import {CouchDB} from "irclog-api";
 type T = "a" | "B";
 let a: T = "a";
 
-const db = new CouchDB("https://db.softver.org.mk", "irclog");
+const db = new CouchDB("https://db.softver.org.mk/irclog/", "_design/log/_view/channel");
 let res = await db.fetchChannelList();
 console.log(res);
